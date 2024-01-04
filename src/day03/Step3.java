291188b048fb59832b1cee4f8f5df43d0bc061aa
itdanja <itdanja@kakao.com>
2024-01-04 14:13:25 +0900

    20240104 연산자 17문제

--- src/day03/Step3.java
+++ src/day03/Step3.java
@@ -1,5 +1,6 @@
 package day03;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Step3 {
@@ -49,31 +50,90 @@ public class Step3 {
         double 퍼센트= 실수1/실수2*100;
         System.out.printf("퍼센트:%.1f %%",퍼센트);
 
-        // 8[김민지].사다리꼴 넓이 구하기[윗변과 밑변 높이를 입력받아 출력하기 , 소수점 첫째 자리까지 출력하시오.
+        // 8.[김민지].사다리꼴 넓이 구하기[윗변과 밑변 높이를 입력받아 출력하기 , 소수점 첫째 자리까지 출력하시오.
         //        계산식) 사다리꼴 계산식 = > (윗변 + 밑변) * 높이 / 2
         System.out.println("윗변:"); double a= scanner.nextDouble();
         System.out.println("밑변:"); double b= scanner.nextDouble();
         System.out.println("높이:"); double h= scanner.nextDouble();
         System.out.printf("넓이: %.1f", (a+b)*h/2);
 */
-        // 9.
+        // 9.[장은경]키를 정수를 입력받아 표준체중 출력하기 , 정수형으로 출력하시오.
+        //        계산식) 표준체중 계산식 = > (키 - 100) * 0.9
+        /*System.out.print("키를 입력하세요 : ");
+        int h=scanner.nextInt();
 
-        // 10.
+        int 표준체중=(int)((h-100)*0.9);
 
-        // 11.
+        System.out.println("표준체중 :"+표준체중);*/
 
-        // 12.
+        // 10.[박효성] 키와 몸무게를 입력받아 BMI 출력하기 , 소수점 둘째 자리 까지 출력하시오.
+        //        계산식) BMI 계산식 = > 몸무게 / ((키 / 100) * (키 / 100))
+        /*System.out.print("키를 입력하세요 : "); double h=scanner.nextDouble();
+        System.out.print("몸무게를 입력하세요 : "); double w=scanner.nextDouble();
 
-        // 13.
+        System.out.printf("BMI: %.2f",w/((h/100)*(h/100)));*/
+        // 11. []
+//        System.out.print("inch : "); double inch = scanner.nextDouble();
+//        System.out.printf("cm : %.2f",inch * 2.54);
 
-        // 14.
+        // 12. [전승호]문제12:  중간고사, 기말고사, 수행평가를 입력받아 반영비율별 계산하여 소수 둘째자리까지 점수 출력하시오
+        //        계산식 반영비율)  중간고사 반영비율 => 30 %  / 기말고사 반영비율 => 30 %   / 수행평가 반영비율 => 40 %
+//        System.out.print("중간고사 점수 입력해주세요 : ");
+//        double a1 =scanner.nextDouble();
+//        System.out.print("기말고사 점수 입력해주세요 : ");
+//        double a2 =scanner.nextDouble();
+//        System.out.print("수행평가 점수 입력해주세요 : ");
+//        double a3 =scanner.nextDouble();
+//        System.out.printf("중간고사 = %.2f 기말고사 = %.2f 수행평가 = %.2f\n",a1*0.3,a2*0.3,a3*0.4);
 
-        // 15.
+        // 13. [ 심재훈 ]
+//        문제13 :  연산 순서 나열 하고 printf() 에 출력되는 x 와 y 값을 예측하시오.
+//        int x = 10;  1. x에 10 대입
+//        int y = x-- + 5 + --x;
+//        우항먼저: 2.x-- 호출: x에 현재값 10 호출 후 x -= 1 실행 현재 x값 = 9
+//                  3. --x 호출: x-=1 실행후 x의 현재값(8)호출 현재 x값 = 8
+//                  4.10+5+8 산술연산 = 23
+//                  5.y에 4의 값 23 대입
+//        printf(" x의 값 : %d , y의값 :  %d ", x, y)
+//        따라서 x= 8, y=23 출력
+        /*
+            x-- + 5 + --x;
+            1. x-- + 5  -> 10 + 5  =>   15     그다음에 x차감   [ 더한후에 차감 ]
+            2. 15 + --x -> [ 더하기전에 차감 ] 15 + 8 => y = 23 , x = 8
+        */
+        // 14.[이민형]문제14 : 나이를 입력받아 나이가 10세이상이면 학생 , 20세이상이면 성인 , 40세이상이면 중년 으로 출력하기
+//        System.out.print("나이 : ");
+//        int 나이 = scanner.nextInt();
+//        System.out.println(나이 >= 40 ? "중년" : 나이 >= 20 ? "성인" : 나이 >= 10 ? "학생" : "아이");
 
-        // 16.
+        // 15.[신승호] 문제15 : 국어 , 영어 , 수학 점수를 입력받아 각 변수에 저장하고 총점(소수점 0자리) 출력 , 평균(소수점 2자리 까지) 출력
+       /* System.out.print("국어 : ");
+        int 국어 = scanner.nextInt();
+        System.out.print("영어 : ");
+        int 영어 = scanner.nextInt();
+        System.out.print("수학 : ");
+        int 수학 = scanner.nextInt();
 
-        // 17.
+        int 총점 = 국어+영어+수학;
+        System.out.println("총점 = " + 총점);
+        double 평균 = (int)(총점/3.0*100)/100.0;
+        System.out.println("평균 = " + 평균);
+*/
+        // 16. [ 노진섭 ]
+        //문제16 : 아이디[문자열] 와 비밀번호[문자열] 를 입력받아 아이디가 'admin' 이고 비밀번호가 '1234' 와 일치하면 '로그인성공' 아니면 '로그인실패' 출력
+        System.out.print("아이디 : ");String 아이디 = scanner.nextLine();
+        System.out.print("비밀번호 : ");String 비번 = scanner.nextLine();
 
+        System.out.println("로그인 정보 = " + (아이디.equals("admin") && 비번.equals("1234") ? "로그인성공" : "로그인실패"));
+
+        // 17. [ 차준영 , 신승호  ]
+//        System.out.println("정수4 입력 : ");
+//       int a = scanner.nextInt();
+//        System.out.println("정수5 입력 : ");
+//        int b = scanner.nextInt();
+//        System.out.println("정수6 입력 : ");
+//        int c = scanner.nextInt();
+//        System.out.println( a >=b && a>=c ? a : b>=a && b>=c ? b : c);
 
 
     }
