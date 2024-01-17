0c10cd7f79e1b7b7cfa954e91ccdac7be81ebc09
itdanja <itdanja@kakao.com>
2024-01-17 12:25:28 +0900

    20240117 회원MVC

--- src/day12/model/MemberDao.java
+++ src/day12/model/MemberDao.java
@@ -2,6 +2,8 @@ package day12.model;
 
 import day12.controller.MemberController;
 
+import java.util.ArrayList;
+
 // DAO : 데이터베이스 접근 객체
 public class MemberDao {
     // 싱글톤
@@ -10,17 +12,30 @@ public class MemberDao {
     public static MemberDao getInstance(){ return memberDao; }
 
     // DB 대신에 배열
-    MemberDto[] memberDtos = new MemberDto[100];
+        // 배열 사용시 불편한점 : 고정길이
+        // 고정길이 배열을 가변길이 배열로 구현 => 제공하는 라이브러리/컬렉션(수집) 프레임워크(라이브러리집합)
+        // 컬렉션 프레임워크 : ArrayList 가변길이 배열을 지원하는 클래스
+        // ArrayList<여러개저장할객체의타입> 변수명 = new ArrayList<>();
+        // 제공하는 함수
+                // 저장 : java[ .add( 저장할 객체 ) ]  vs  js[ .push( 저장할 데이터 ) ]
+    // MemberDto[] memberDtos = new MemberDto[100];
+    ArrayList<MemberDto> memberDtos = new ArrayList<>();
 
     // 1. 회원가입 메소드
     public boolean signup( MemberDto memberDto ){
         System.out.println("MemberDao.signup");
-        for( int i = 0 ; i < memberDtos.length ; i++ ){
-            if( memberDtos[i] == null ){
-                memberDtos[i] = memberDto;
-                return true; // 회원가입 성공
-            }
-        }
-        return false; // 회원가입 실패
+
+        memberDtos.add( memberDto );
+
+        return true; // 회원가입 성공
     } // f end
 }
+
+/*
+for( int i = 0 ; i < memberDtos.length ; i++ ){
+    if( memberDtos[i] == null ){
+        memberDtos[i] = memberDto;
+        return true; // 회원가입 성공
+    }
+}
+*/
